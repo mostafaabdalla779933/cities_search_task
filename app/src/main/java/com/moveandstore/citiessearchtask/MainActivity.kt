99f9374438c07bda6859b97a0f2cc3ac65b2c4c0
Mustafa Abdulaziz <mustafaabdulaziz@paymob.com>
2025-04-27 06:44:15 +0300

    init commit

---
+++ app/src/main/java/com/moveandstore/citiessearchtask/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.moveandstore.citiessearchtask
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.moveandstore.citiessearchtask.ui.theme.CitiesSearchTaskTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CitiesSearchTaskTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CitiesSearchTaskTheme {
+        Greeting("Android")
+    }
+}
