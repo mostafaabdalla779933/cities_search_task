5c45d3ee8692df78dbb8911e0c17a57fdc2aa9a0
Mustafa Abdulaziz <mustafaabdulaziz@paymob.com>
2025-04-28 14:31:14 +0300

    - Add comment to clarify why we choose Trie data structure

--- app/src/main/java/com/moveandstore/citiessearchtask/data/trie/CityTrie.kt
+++ app/src/main/java/com/moveandstore/citiessearchtask/data/trie/CityTrie.kt
@@ -2,6 +2,11 @@ package com.moveandstore.citiessearchtask.data.trie
 
 import com.moveandstore.citiessearchtask.data.model.City
 
+/**
+In the beginning, I was undecided between two data structures: TreeMap and Trie.
+I ultimately preferred using Trie because it offers faster performance for autocomplete and prefix-based string search operations.
+However, the main drawback of Trie is that it consumes more memory (RAM) compared to other structures like TreeMap.
+ */
 
 class CityTrie {
     private val root = TrieNode()
